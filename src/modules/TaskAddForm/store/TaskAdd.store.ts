6b11d4cd334b9ea0a548125e12ab2f3e03d95baa
Zhirayr <jirayr.dabaxyn@mail.ru>
2023-04-11 16:32:58 +0300

    first commit

---
+++ src/modules/TaskAddForm/store/TaskAdd.store.ts
@@ -0,0 +1,36 @@
+import { action, computed, makeObservable, observable } from 'mobx';
+import { TaskEntityAdd } from 'domains/index';
+import { TaskAddAgentInstance } from 'http/agent/TaskAdd.agent';
+import { mapToExternalTask } from 'helpers/mappers';
+type PrivateFields = '_task';
+
+export class TaskAddStore {
+  constructor() {
+    makeObservable<this, PrivateFields>(this, {
+      _task: observable,
+      task: computed,
+      createTask: action,
+    });
+  }
+
+  private _task: TaskEntityAdd = {
+    name: '',
+    info: '',
+    isImportant: false,
+  };
+
+  get task(): TaskEntityAdd {
+    return this._task;
+  }
+  createTask = async (newTask: TaskEntityAdd) => {
+    try {
+      const data = mapToExternalTask(newTask);
+      await TaskAddAgentInstance.createTask(data);
+    } catch {
+      console.log('Задача не создана!');
+    }
+    return true;
+  };
+}
+
+export const TaskAddStoreInstance = new TaskAddStore();
